54639870ea00f536374cb2280d0982a5c1d89384
ryuuki-sasaki <r.ryuuki.sasaki@gmail.com>
2023-04-29 20:24:11 +0900

    feat: webから取得したページの内容から記事作成に必要な情報を生成する

--- index.py
+++ index.py
@@ -1,7 +1,22 @@
-from langchain import OpenAI
+import time
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.agents import (
+    load_tools,
+    initialize_agent,
+    AgentType,
+)
 from langchain.document_loaders import SeleniumURLLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.chains import ConversationChain
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
 
 # .env ファイルをロードして環境変数へ反映
 from dotenv import load_dotenv
@@ -9,11 +24,14 @@ load_dotenv()
 import os
 import re
 
+TEXT_SIZE_LIMIT = 4000
+
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 if OPENAI_API_KEY is None:
     raise Exception("OPENAI_API_KEY is not set.")
 
-llm = OpenAI(temperature=0)
+openAI = OpenAI(temperature=0)
+openAIChat = ChatOpenAI(temperature=0)
 
 # urlsを受け取って、Seleniumでページを開いて、ページの内容を返す。
 def GetPageContent(urls):
@@ -30,7 +48,7 @@ def remove_space_and_newline(text):
 # GPT-3による文章要約
 def SummarizeText(docs):
     #  Documentsを並列で要約するmap_reduceの方が早いが、今回は要約の精度重視でrefineを使う。
-    chain = load_summarize_chain(llm, chain_type="refine")
+    chain = load_summarize_chain(openAI, chain_type="refine")
     summary = chain.run(docs)
     return summary
 
@@ -46,8 +64,37 @@ def GenerateArticle():
     )
     docs = text_splitter.create_documents([text])
 
-    # 文章を要約する
-    summary = SummarizeText(docs)
-    print(summary)
+    # テキストのサイズが定数文字を超えるなら文章を要約する
+    summary = ""
+    if len(text) > TEXT_SIZE_LIMIT:
+        summary = SummarizeText(docs)
+
+    # systemメッセージプロンプトテンプレートの準備
+    template="これからテキストを複数に分割して渡しますので、私が「まとめてください」というまでは、作業を始めないでください。代わりに「次の入力を待っています」とだけ出力してください。"
+    # chatプロンプトテンプレートの準備
+    prompt = ChatPromptTemplate.from_messages([
+        SystemMessagePromptTemplate.from_template(template),
+        MessagesPlaceholder(variable_name="history"),
+        HumanMessagePromptTemplate.from_template("{input}")
+    ])
+    # メモリの準備(トークン上限は2000,超過した分は要約される)
+    memory = ConversationSummaryBufferMemory(llm=openAI, max_token_limit=2000, return_messages=True)
+    conversation_with_summary = ConversationChain(
+        llm=openAIChat, 
+        prompt=prompt,
+        memory=memory,
+        verbose=True,
+    )
+    
+    if summary != "":
+        conversation_with_summary.predict(input=summary)
+    else:
+        for doc in docs:
+            conversation_with_summary.predict(input=doc.page_content + "\n\n上記の文章は全体のテキストの一部です。まだまとめないでください")
+    conversation_with_summary.predict(input="これで全ての文章を渡しました。これらをまとめてください。")
+    res = conversation_with_summary.predict(input="この文章のキーワードを重要度の高い順に3つ挙げてください。")
+    print(res)
+
+
 
 GenerateArticle()
